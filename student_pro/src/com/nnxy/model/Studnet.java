207729c566f5d1cb04e03366751af3e1c7dcf5af
huangbangyi2019 <1094476088@qq.com>
2019-05-16 16:26:06 +0800

    初始化工程

---
+++ student_pro/src/com/nnxy/model/Studnet.java
@@ -0,0 +1,46 @@
+package com.nnxy.model;
+
+public class Studnet {
+
+	private int id;
+	private String name;
+	private int age;
+	private int sex;
+	
+	
+	public Studnet(int id, String name, int age, int sex) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	
+	
+	
+}
